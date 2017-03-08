3b5d3a3ebd8554ebb0f615e60a727b924547ec12
Oleksii <kiyanov.alex@gmail.com>
2017-03-08 21:05:26 +0200

    add console.log

--- script.js
+++ script.js
@@ -9,13 +9,17 @@ window.onload = function() {
 			
 			data.open('GET', 'users.json', true);
 			data.send();
-			data.onreadystatechange = function(){  
+			data.onreadystatechange = function(){
+
 				if (data.readyState != 4) return;
 				
 				if (data.status != 200) {
 				alert(data.status + ': ' + data.statusText);
 			  } else {
-				alert(data.responseText);
+				let users = JSON.parse(data.responseText);
+					users.forEach((user) => {
+						console.log(user.name)
+					})
 			  }
 			
 			}			
