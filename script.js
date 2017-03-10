e5ac10e79af38ba79598f82e54a4014b0b136e88
Mikhail Koshmay <michail_koshmay@mail.ru>
2017-03-10 17:53:13 +0200

    'style.css'

--- script.js
+++ script.js
@@ -17,12 +17,39 @@ window.onload = function() {
 				alert(data.status + ': ' + data.statusText);
 			  } else {
 				let users = JSON.parse(data.responseText);
+					let tbody = document.getElementById("tableId");
+										
 					users.forEach((user) => {
-						console.log(user.name)
-					})
-			  }
-			
-			}			
-					
+						console.log(user.name);
+							let tr = document.createElement('tr');
+						
+								let td1 = document.createElement('td');
+								td1.innerHTML = user.name;
+								tr.appendChild(td1);
+								
+								let td2 = document.createElement('td');
+								td2.innerHTML = user.gender;
+								td2.classList.add("genderCompanyAge");
+								tr.appendChild(td2);
+								
+								let td3 = document.createElement('td');
+								td3.innerHTML = user.company;
+								td3.classList.add("genderCompanyAge");
+								tr.appendChild(td3);
+								
+								let td4 = document.createElement('td');
+								td4.innerHTML = user.age;
+								td4.classList.add("genderCompanyAge");
+								tr.appendChild(td4);
+								
+								let td5 = document.createElement('td');
+								td5.innerHTML = user.email;
+								td5.classList.add("mail");
+								tr.appendChild(td5);
+												
+							tbody.appendChild(tr);
+					});
+			  }			
+			}							
 	}	
 			
