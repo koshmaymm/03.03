6616014f7895b727005b4f068102c1d466be7449
Mikhail Koshmay <michail_koshmay@mail.ru>
2017-03-10 19:53:28 +0200

    cycle worked

--- script.js
+++ script.js
@@ -15,28 +15,24 @@ window.onload = function() {
 				
 				if (data.status != 200) {
 				alert(data.status + ': ' + data.statusText);
-			  } else {
-				let users = JSON.parse(data.responseText);
-					let tbody = document.getElementById("tableId");
+				} else {
+							let users = JSON.parse(data.responseText);
+							let tableBody = document.getElementById("tableId");			
+							users.forEach((user) => {
+								console.log(user.name);
+									let tr = document.createElement('tr');
+									let usersArray = [user.name, user.gender, user.company, user.age, user.email]
+							
+										for (let a = 0; a < usersArray.length; a++){ 
+											  let td = document.createElement('td');
+											  td.innerHTML = usersArray[a];
+											  if (usersArray[a] == user.gender || user.company || user.age){td.classList.add("genderCompanyAge");}
+											  tr.appendChild(td);
+										} 				
 										
-					users.forEach((user) => {
-						console.log(user.name);
-							let tr = document.createElement('tr');
-							let usersArray = [user.name, user.gender, user.company, user.age, user.email]
-						
-									for (let a = 0; a < usersArray.length; a++){ 
-										  let td = document.createElement('td');
-										  td.innerHTML = usersArray[a];
-										  if (usersArray[a] == user.gender){td.classList.add("genderCompanyAge");}
-										  if (usersArray[a] == user.company){td.classList.add("genderCompanyAge");}
-										  if (usersArray[a] == user.age){td.classList.add("genderCompanyAge");}
-										  tr.appendChild(td);
-									}
-									
-									}
-																
-							tbody.appendChild(tr);
-					});
+							tableBody.appendChild(tr);											
+								
+						});
 			  }			
 			}							
 	}	
