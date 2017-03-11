ab51754218855046d8dcca534ae57fd6c9876b6d
Mikhail Koshmay <michail_koshmay@mail.ru>
2017-03-11 17:47:25 +0200

    function not all

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-
 window.onload = function() {
     	let button = document.getElementById("but");
 		button.addEventListener("click", showData, false);
 		let buttonBones = document.getElementById("bones");
 		buttonBones.addEventListener("click", throwBones, false);
+		let bonesStatistics = document.getElementById("bonesStatistics");
+		bonesStatistics.addEventListener("click", showBonesStatistics, false);
+		
 };
   
 	function showData(){
@@ -38,20 +40,61 @@ window.onload = function() {
 			  }			
 			}							
 	}	
-		
-		
+				
 	function throwBones(){
-		
 				let min = 1, max = 6;
 				let bonesExit = document.getElementById("bonesExit");
 				randomDice(min, max);
 	}	
 	function randomDice(min, max) {
 				let rand = Math.floor(min + Math.random() * (max + 1 - min));
-				let rand2 = Math.floor(min + Math.random() * (max + 1 - min));
-									
+				let rand2 = Math.floor(min + Math.random() * (max + 1 - min));						
 				rendertThrowBonesResult(rand, rand2); 	  
 	}
 	function rendertThrowBonesResult(a, b){
 				bonesExit.innerHTML = "Выпали цифры " + a + " и " + b;
+	}
+	
+	function showBonesStatistics(){
+				let statisticsArray = [2,3,4,5,6,7,8,9,10,11,12];
+				let resArray = [0,0,0,0,0,0,0,0,0,0,0];
+				
+				
+				//console.log(statisticsArray);
+				//console.log(resArray);
+				numberStartsStatistics(1,6,450);
+	
+	
+			function numberStartsStatistics(a,b,c){
+						for (let i = 0; i < c; i++) {
+						  randomStatistics(a,b);
+						}
+					console.log(resArray);
+					
+					showStatistics();
+			}
+			
+			function randomStatistics(min, max) {
+						let rand = Math.floor(min + Math.random() * (max + 1 - min));
+						let rand2 = Math.floor(min + Math.random() * (max + 1 - min));						
+						let res = rand + rand2;
+						
+								for (let i = 0; i < statisticsArray.length; i++) {
+									  if (statisticsArray[i] == (res) ) { resArray[i]++; }
+								}
+								
+			}
+			
+			function showStatistics(){
+				
+				let li = document.createElement("li");
+				li.innerHTML = "Статистика выпадания цифр: ";
+				list.appendChild(li);
+				
+				for (let i = 0; i < statisticsArray.length; i++) {
+					let li = document.createElement("li");
+					li.innerHTML = statisticsArray[i] + " Выпадало " + resArray[i] + " раз ";
+					list.appendChild(li);				  
+				}
+			}
 	}
